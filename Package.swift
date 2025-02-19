f6e0e86df47702678e41e47395608026f5a1d03a
Manu <manuel.g@connectif.es>
2025-02-19 13:50:39 +0100

    Package.swift for version 1.0.1

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ConnectifSDK",
-            url: "https://github.com/connectif/Connectif-iOS-SDK/releases/download/1.0.0/ConnectifSDK.xcframework.zip",
-            checksum: "e71f49434c7825d7605830f9cf7c314d88cf0f8ca5a30eb4ac57726fc4de8446"
+            url: "https://github.com/connectif/Connectif-iOS-SDK/releases/download/1.0.1/ConnectifSDK.xcframework.zip",
+            checksum: "6fa78e57a2f6eddf03f4a64a93d0da261ff8fe21c2bb626976135016728900dd"
         )
     ]
 )
